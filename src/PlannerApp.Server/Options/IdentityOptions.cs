04e6900812e361d82d306557e7bfb281e6bcaa7b
Ahmad <aksoftware98@hotmail.com>
2021-04-30 16:15:52 +0300

    Implement IdentityOptions

---
+++ src/PlannerApp.Server/Options/IdentityOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlannerApp.Server.Options
+{
+    public class IdentityOptions
+    {
+
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
