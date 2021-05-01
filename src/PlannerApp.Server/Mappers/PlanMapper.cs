665438af8073cb67da4fc86545aae7b938580850
Ahmad <aksoftware98@hotmail.com>
2021-05-02 02:28:12 +0300

    Create the model mappers

---
+++ src/PlannerApp.Server/Mappers/PlanMapper.cs
@@ -0,0 +1,26 @@
+﻿using PlannerApp.Models;
+using PlannerApp.Models.V2.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlannerApp.Server.Mappers
+{
+    public static class PlanMapper
+    {
+
+        public static PlanDetail ToPlanDetail(this Plan model)
+        {
+            return new PlanDetail
+            {
+                Id = model.Id,
+                CoverUrl = model.CoverPath,
+                Description = model.Description,
+                Title = model.Title,
+                ToDoItems = model.ToDoItems?.Select(t => t.ToToDoItemDetail()).ToList()
+            };
+        }
+
+    }
+}
---
+++ src/PlannerApp.Server/Mappers/ToDoItemMapper.cs
@@ -0,0 +1,21 @@
+﻿using PlannerApp.Models;
+using PlannerApp.Models.V2.DTO;
+
+namespace PlannerApp.Server.Mappers
+{
+    public static class ToDoItemMapper
+    {
+        public static ToDoItemDetail ToToDoItemDetail(this ToDoItem model)
+        {
+            return new ToDoItemDetail
+            {
+                Id = model.Id,
+                Description = model.Description,
+                AchievedDate = model.AchievedDate,
+                EstimationDate = model.EstimatedDate,
+                IsDone = model.IsDone,
+                PlanId = model.PlanId
+            };
+        }
+    }
+}
